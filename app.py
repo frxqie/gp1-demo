6da986c5a07064c77aca80faa3999b9bcb617509
Farah AbuAjameieh <farahajamieh2004@gmail.com>
2026-04-30 21:12:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def home():
         except:
             result = "Invalid input"
 
-    return render_template("index.html", result=result)
+    return render_template("template/index.html", result=result)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
